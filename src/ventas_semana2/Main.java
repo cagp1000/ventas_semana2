0aa8a8063154e2745fe0b243cf15a1468ed7a2b9
frodriguezu <frodriguezu@poligran.edu.co>
2024-04-11 22:16:55 -0500

    modificación método createSalesMenFile creación archivo por vendedor

--- src/ventas_semana2/Main.java
+++ src/ventas_semana2/Main.java
@@ -127,31 +127,47 @@ class GenerateInfoFiles {
 		return dataSellers;
 	}
 	
-	public String createSalesMenFile(String ruta) {		
+	public String createSalesMenFile(String rutaVentasGeneradas, String[][] resultListSales) {
 		// Generar archivos de ventas
 		try {
-	    	String columnas = "TipoDocumentoVendedor;NúmeroDocumentoVendedor;IDProducto;CantidadProductoVendido\n";
-	    	String vendedor1 = "CC;18397655;6;4\n";
-	    	String vendedor2 = "CC;52345243;2;5\n";
-	    	String vendedor3 = "CC;24354267;3;1\n";
-	    	String vendedor4 = "CC;126776666;1;6\n";
-	    	String vendedor5 = "CC;53155393;5;3\n";
-	    	File file = new File(ruta);
-	    	if (!file.exists()) {
-		    	file.createNewFile();
-		    	FileWriter fw = new FileWriter(file);
-		    	BufferedWriter bw = new BufferedWriter(fw);
-		    	bw.write(columnas);
-		    	bw.write(vendedor1);
-		    	bw.write(vendedor2);
-		    	bw.write(vendedor3);
-		    	bw.write(vendedor4);
-		    	bw.write(vendedor5);
-		    	bw.close();
-		    	response = "Archivo de ventas generado!";
-	    	} else {
-	    		response = "Archivo de ventas ya existe!";
-	    	}
+			int counterFinal = 0; // Variable para verificar que se ralicen los 5 archivos
+			int counterUser = 1; // Variable para identificar cuando se acaba listado de ventas
+			String sellsUser = ""; // Variable que almacena en string las ventas de cada vendedor
+			int index = 1; // Variable que siempre va a traer valor de No documento del array por vendedor
+			 
+			// Armar variable con los datos y generar archivo por cada vendedor
+			for (String[] lista : resultListSales) {
+				for (int i = 0; i < lista.length; i++) {
+					if (sellsUser == "") {
+						sellsUser = lista[i];
+					} else if (counterUser % 2 != 0) {
+						sellsUser += lista[i];
+					} else {
+						sellsUser += ";" + lista[i] + "\n";
+					}
+					counterUser += 1;
+					if (counterUser == 13) {
+						String archivo = rutaVentasGeneradas + lista[index] + ".txt";
+						File file = new File(archivo);
+						file.createNewFile();
+						FileWriter fw = new FileWriter(file);
+						BufferedWriter bw = new BufferedWriter(fw);
+						bw.write(sellsUser);
+						bw.close();
+						counterUser = 1;
+						sellsUser = "";
+						counterFinal += 1;
+					}
+				}
+			}
+			
+			
+			// Verificar que los 5 archivos han sido generados
+			if (counterFinal==5) {
+				response = "Archivos de ventas por vendedores generados!";
+			} else {
+				response = "Error al crear uno o varios archivos de ventas por vendedores generados!";
+			}
     	} catch (Exception e) {
     		StringWriter sw = new StringWriter();
     		PrintWriter pw = new PrintWriter(sw);
@@ -182,13 +198,12 @@ public class Main {
     	
     	// Generar lista de ventas para generar archivo de ventas
     	String[][] resultListSales = ventas.createListSales(resultProducts, resultSellers);
-    	System.out.println(Arrays.deepToString(resultListSales));
+    	// System.out.println(Arrays.deepToString(resultListSales));
 		
 		// Generar archivo ventas_generadas.txt
-		String rutaVentasGeneradas = "ventas_generadas.txt";
-		GenerateInfoFiles genSellsFile = new GenerateInfoFiles();
-		System.out.println(genSellsFile.createSalesMenFile(rutaVentasGeneradas));
-//		System.out.println(genSellsFile.createSalesMenFile(rutaProductosGenerados, rutaVendedoresGenerados));
+    	String rutaVentasGeneradas = "ventas_generadas_";
+    	String resultSalesFile = ventas.createSalesMenFile(rutaVentasGeneradas, resultListSales);
+    	System.out.println(resultSalesFile);
 	}
 
 }
